44fa2cf842a2ca779644dadd99b8b2f08d957ef0
ashraf1248 <146019219+ashraf1248@users.noreply.github.com>
2024-01-14 23:13:05 -0500

    Update stock_alert.py

--- stock_alert.py
+++ stock_alert.py
@@ -2,7 +2,7 @@ import os
 from twilio.rest import Client
 from alpha_vantage.timeseries import TimeSeries
 def get_latest_stock_price(stock_symbol):
-  alpha_vantage_api_key = "OUGFTWVBURPMJJS8"
+  alpha_vantage_api_key = "Key"
   alpha_vantage = TimeSeries(alpha_vantage_api_key)
   try:
     data,_ = alpha_vantage.get_quote_endpoint(symbol=stock_symbol)
@@ -12,10 +12,10 @@ def get_latest_stock_price(stock_symbol):
     print(f"Error: {e}")
 
 def send_stock_alert(symbol,price):
-  account_sid = 'ACc30102ee4a14976b26f48a2b24af6452'
-  auth_token = '8d6b83ddde19283c2cff84b772efaab6'
-  twilio_num = '+18333402261'
-  your_num = '+19084046643'
+  account_sid = 'ID'
+  auth_token = 'TOKEN'
+  twilio_num = 'NUM'
+  your_num = 'NUM'
   client = Client(account_sid, auth_token)
   message = f"Stock price of {symbol} is now {price}. Time to buy or sell."
   print(f"Stock price of {symbol} is now {price}. Time to buy or sell.")
@@ -27,13 +27,7 @@ def send_stock_alert(symbol,price):
 
 def main():
   stock_symbol = 'AAPL'
-  alert_threshold = 10.0
   latest_price = get_latest_stock_price(stock_symbol)
   send_stock_alert(stock_symbol, latest_price)
 
-  '''
-  if latest_price < alert_threshold:
-    send_stock_alert(stock_symbol, latest_price)
-  else:
-    print("No alert triggered.")'''
 main()
